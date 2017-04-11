6926303c392d385494a408832cc7fa439f9bc7d1
AstakhovAnton <aga.26@yandex.ru>
2017-04-11 14:26:06 +0300

    test_of_library

--- Test_of_networkx.py
+++ Test_of_networkx.py
@@ -17,3 +17,12 @@ for i in range(n):
     print(g.neighbors(i))
 #print()
 #print(nx.degree(g, 1))
+MG=nx.MultiGraph()
+MG.add_weighted_edges_from([(1,2,.5), (1,2,.75), (2,3,.5)])
+MG.degree(weight='weight')
+GG=nx.Graph()
+for n,nbrs in MG.adjacency_iter():
+    for nbr,edict in nbrs.items():
+        minvalue=min([d['weight'] for d in edict.values()])
+        GG.add_edge(n,nbr, weight = minvalue)
+nx.shortest_path(GG,1,3)
