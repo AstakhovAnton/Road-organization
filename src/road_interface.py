de88e174d45a4f8aaa3c991aabb0cc8cbffebb77
kristinaivanova97 <krapinka_krapinka@mail.ru>
2017-04-17 22:20:01 +0300

    Улучшенный интерфейс

--- src/road_interface.py
+++ src/road_interface.py
@@ -1,9 +1,15 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QTextEdit, QAction, QMessageBox
-from PyQt5.QtWidgets import (QWidget, QToolTip,
-                             QPushButton, QApplication, QHBoxLayout, QVBoxLayout)
-from PyQt5.QtGui import QFont, QIcon
+#from PyQt5.QtWidgets import QMainWindow, QTextEdit, QAction, QMessageBox
+#from PyQt5.QtWidgets import (QWidget, QToolTip,
+#                             QPushButton, QApplication, QHBoxLayout, QVBoxLayout)
+#from PyQt5.QtGui import QFont, QIcon
 
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+
+from Slava import Slava
 
 class Example(QMainWindow):
     def __init__(self):
@@ -12,13 +18,53 @@ class Example(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        ##textEdit = QTextEdit()
-        ##self.setCentralWidget(textEdit)
+
+        self.widget = Slava.CurveDrawer(self)
+        print(self.widget.vertices)
+        self.setCentralWidget(self.widget)
+
+        col = QColor(0, 0, 0)
+
+        mydocwidget = QDockWidget('colour', self)
+
+        btn = QPushButton('colour', self)
+        btn.resize(btn.sizeHint())
+
+
+        btn.clicked.connect(self.showDialog)
+
+        mydocwidget.setWidget(btn)
+
+        self.addDockWidget(Qt.BottomDockWidgetArea, mydocwidget)
+
+        self.widget.setStyleSheet("QWidget { background-color: %s }"
+                                    % col.name())
+
 
         exitAction = QAction(QIcon('exit2.png'), 'Exit', self)
         exitAction.setShortcut('Ctrl+Q')
         exitAction.setStatusTip('Exit application')
         exitAction.triggered.connect(self.close)
+
+        previousAction = QAction(QIcon('left.png'), 'previous', self)
+        previousAction.setShortcut('F4')
+        previousAction.setStatusTip('previous')
+        previousAction.triggered.connect(self.close)
+
+        nextAction = QAction(QIcon('right.png'), 'next', self)
+        nextAction.setShortcut('F3')
+        nextAction.setStatusTip('next')
+        nextAction.triggered.connect(self.close)
+
+        saveAction = QAction(QIcon('save.png'), 'save', self)
+        saveAction.setShortcut('F2')
+        saveAction.setStatusTip('save current file')
+        saveAction.triggered.connect(self.save)
+
+        downloadAction = QAction(QIcon('globe.png'), 'Download', self)
+        downloadAction.setShortcut('F5')
+        downloadAction.setStatusTip('Download road network')
+        downloadAction.triggered.connect(self.close)
 
         self.statusBar()
 
@@ -29,33 +75,25 @@ class Example(QMainWindow):
         toolbar = self.addToolBar('Exit')
         toolbar.addAction(exitAction)
 
-        #toolbar2 = self.addToolBar('Previous')
+        toolbar2 = self.addToolBar('Previous')
+        toolbar2.addAction(previousAction)
+
+        toolbar3 = self.addToolBar('next')
+        toolbar3.addAction(nextAction)
+
+        toolbar4 = self.addToolBar('Save')
+        toolbar4.addAction(saveAction)
+
+        toolbar5 = self.addToolBar('Download')
+        toolbar5.addAction(downloadAction)
 
         QToolTip.setFont(QFont('SansSerif', 10))
-
-        self.setToolTip('This is a <b>QWidget</b> widget')
-
-        btn = QPushButton('Button', self)
-        btn.setToolTip('This is a <b>QPushButton</b> widget')
-        btn.resize(btn.sizeHint())
-        btn.move(350, 300)
-
-        hbox = QHBoxLayout()
-        hbox.addStretch(1)
-        hbox.addWidget(btn)
-
-        vbox = QVBoxLayout()
-        #vbox.addMenuBar(menubar)
-        #vbox.add
-        vbox.addStretch(1)
-        vbox.addLayout(hbox)
-
-        self.setLayout(vbox)
-
 
         self.setGeometry(400, 300, 450, 350)
         self.setWindowTitle('Main window')
         self.show()
+
+
 
     def closeEvent(self, event):
 
@@ -67,8 +105,24 @@ class Example(QMainWindow):
             event.accept()
         else:
             event.ignore()
+    def save(self):
+        fd = open('Saves.txt', 'w')
+        verticeslist = self.widget.vertices
+
+        coordlist = []
+        for vertex in verticeslist:
+            coordlist.append((vertex.x(), vertex.y()))
+        for cortege in coordlist:
+            fd.write(str(cortege) + ' ')
+        fd.close()
 
 
+    def showDialog(self):
+        col = QColorDialog.getColor()
+
+        if col.isValid():
+            self.widget.setStyleSheet("QWidget { background-color: %s }"
+                                    % col.name())
 
 
 if __name__ == '__main__':
