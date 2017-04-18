d849a0d84b6bcbfff9449fb01cedc11aa2e8045d
IvanovaVA <valeryank_a@mail.ru>
2017-04-18 10:09:04 +0300

    car

--- src/Car.py
+++ src/Car.py
@@ -1,32 +1,32 @@
 import time
-
-class Road:
-    def __init__(self, listx, listy):
-        self.points = []
-        for i in range(len(listx)):
-            self.points.append((listx[i], listy[i]))
-
-    def roadStart(self):
-        return self.points[0]
-
+import networkx as nx
+from Road import Road
 
 class Car:
     def __init__(self, v):
+        self.velocity = v
+        self.current_vertex = 1
+        self.finish_vertex = 2
 
-         self.velocity = v
+    def way(self, net):
+        return (nx.dijkstra_path(net.matrix, self.current_vertex, self.finish_vertex))[1]
 
-    def searchForTheBestWay(self, graph):
-         pass
-
-    def movement(self, road):
-        self.road = road
+    def movement(self, net):
+        where = self.way(net)
+        print(where)
+        road = net.mat_object[self.current_vertex][where]
         for i in range(len(road.points)):
             time.sleep(1/self.velocity)
             self.current_point = road.points[i]
-            print(self.current_point)
+            ##print(self.current_point)
+            if self.finish_vertex == where:
+                return
+        self.current_vertex = where
+        self.movement(net)
 
 if __name__ == '__main__':
 
     road = Road([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])
+    print(road)
     car = Car(2)
     car.movement(road)
