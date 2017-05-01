31d2488d5fc086d10c21be1fa301ca19e6a4ed7e
AstakhovAnton <aga.26@yandex.ru>
2017-05-01 21:33:34 +0300

    Traffic jams, acceleration, deceleration

--- src/Matrix.py
+++ src/Matrix.py
@@ -9,11 +9,11 @@ class Network:
         self.matrix.add_node(name)
 
     def add_edge(self, name1, name2, w, list_points):
-        self.matrix.add_edge(name1, name2, weight=w, dots=list_points)
+        self.matrix.add_edge(name1, name2, weight=w, dots=list_points, on_road=[0] * len(list_points))
 
     def remove_node(self, name):
         self.matrix.remove_node(name)
-    def remove_edge(self, name1, name2, w):
+    def remove_edge(self, name1, name2):
         self.matrix.remove_edge(name1, name2)
 
 '''
@@ -25,6 +25,6 @@ if __name__ == '__main__':
     mynet.add_edge(1, 2, 4, ((1, 1), (2, 2), (3, 3), (4, 4)))
     mynet.add_edge(1, 3, 1, ((1, 1), (2, 2), (3, 3), (4, 4)))
     mynet.add_edge(3, 2, 1, ((1, 1), (2, 2), (3, 3), (4, 4)))
-    mycar = Car(2, 1, )
+    mycar = Car(2, 1, 2)
     mycar.movement(mynet)
 '''
