8bb7f1c8e09051e6cc6898d946ca4957417cb5ee
AstakhovAnton <aga.26@yandex.ru>
2017-05-21 18:35:32 +0300

    way in advance

--- src/Car.py
+++ src/Car.py
@@ -14,11 +14,12 @@ class Car:
         self.drawer = drawer
         self.current_velocity = 5
 
-    def way(self, net):
-        return (nx.dijkstra_path(net.matrix, self.current_vertex, self.finish_vertex))[1]
+    def way(self, net, whence):
+        return (nx.dijkstra_path(net.matrix, whence, self.finish_vertex))[1]
 
     def moveAtoB(self, net, start, finish):
         self.current_vertex = start.name
+        self.start_vertex = start.name
         self.finish_vertex = finish.name
         self.tracker = Tracker()
         self.drawer.carList.append(self.tracker)
@@ -28,22 +29,34 @@ class Car:
         finish.setUnallocated()
 
     def movement(self, net):
-        if self.current_vertex == self.finish_vertex:
+        if self.current_vertex == self.finish_vertex: ## if arrives
             return
 
-        where = self.way(net)
-        best_len = inf
-        best_road = 0
-        for road in net.matrix[self.current_vertex][where].keys():
-            if net.matrix[self.current_vertex][where][road]['weight'] < best_len:
-                best_len = net.matrix[self.current_vertex][where][road]['weight']
-                best_road = road
+        if self.current_vertex == self.start_vertex: ## if starts, where
+            self.where = self.way(net, self.start_vertex)
+            best_len = inf
+            self.best_road = 0
+            for road in net.matrix[self.current_vertex][self.where].keys():
+                if net.matrix[self.current_vertex][self.where][road]['weight'] < best_len:
+                    best_len = net.matrix[self.current_vertex][self.where][road]['weight']
+                    self.best_road = road
 
         vmin = 2
-        distance = len(net.matrix[self.current_vertex][where][best_road]['dots'])
+        future_best_road = 0
+        future_where = self.where
+        distance = len(net.matrix[self.current_vertex][self.where][self.best_road]['dots']) ## traffic
         for i in range(distance):
 
-            if distance < 200:
+            if self.where != self.finish_vertex and distance == i + 9: ## where
+                future_where = self.way(net, self.where)
+                best_len = inf
+                future_best_road = 0
+                for road in net.matrix[self.where][future_where].keys():
+                    if net.matrix[self.where][future_where][road]['weight'] < best_len:
+                        best_len = net.matrix[self.where][future_where][road]['weight']
+                        future_best_road = road
+
+            if distance < 200: ## to determine velocity
                 accel = distance / 2
             else:
                 accel = 100
@@ -58,38 +71,44 @@ class Car:
                 time.sleep(1/self.current_velocity)
 
 
-            no_wait = 0
+            no_wait = 0 ## when may go
             if distance > i + 10:
-                while net.matrix[self.current_vertex][where][best_road]['on_road'][i + 10] == 1:
+                while net.matrix[self.current_vertex][self.where][self.best_road]['on_road'][i + 10] == 1:
                     time.sleep(1/waiting)
             else:
                 while no_wait == 0:
                     no_wait = 1
-                    for close in nx.neighbors(net.matrix, where):
-                        for road in net.matrix[where][close].keys():
+
+
+
+                    for close in nx.neighbors(net.matrix, self.where): ##do not merge
+                        for road in net.matrix[self.where][close].keys():
                             num = 0
-                            for point in net.matrix[where][close][road]['on_road']:
+                            for point in net.matrix[self.where][close][road]['on_road']:
                                 if num > 10 - distance + 1 + i:
                                     break
                                 num += 1
                                 if point == 1:
                                     no_wait *= 0
+
+
                     if no_wait == 0:
                         time.sleep(1/waiting)
 
             #if i < 35 or i > 400:
             #    print(i, self.velocity)
 
-            net.matrix[self.current_vertex][where][best_road]['on_road'][i] = 1
-
+            net.matrix[self.current_vertex][self.where][self.best_road]['on_road'][i] = 1 ## car on road
             if i > 0:
-                     net.matrix[self.current_vertex][where][best_road]['on_road'][i - 1] = 0
+                     net.matrix[self.current_vertex][self.where][self.best_road]['on_road'][i - 1] = 0
 
-            self.current_point = net.matrix[self.current_vertex][where][best_road]['dots'][i]
+            self.current_point = net.matrix[self.current_vertex][self.where][self.best_road]['dots'][i]
             point = Point(self.current_point[0], self.current_point[1])
             self.tracker.setPos(point)
 
-        net.matrix[self.current_vertex][where][best_road]['on_road'][distance - 1] = 0
-        self.current_vertex = where
+        net.matrix[self.current_vertex][self.where][self.best_road]['on_road'][distance - 1] = 0
+        self.current_vertex = self.where
+        self.where = future_where
+        self.best_road = future_best_road
         self.movement(net)
 
